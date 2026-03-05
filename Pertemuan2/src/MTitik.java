19682cbd87e54a4aef4c93ace31af19ed83d3508
anandabayu2611 <anandaputrabayu9@gmail.com>
2026-03-05 13:33:39 +0700

    Bagian 2

--- Pertemuan2/src/MTitik.java
+++ Pertemuan2/src/MTitik.java
@@ -11,5 +11,11 @@
             T1.printTitik();
             T1.geser(3, 4);
             T1.printTitik();
+
+            Titik T2 = T1;
+            T2.printTitik();
+            T1.setAbsis(10);
+            T1.setordinat(10);
+            T2.printTitik();
         }
     }
