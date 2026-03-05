54bb736fd4029aae9fbab699faaab0f2028bde2f
anandabayu2611 <anandaputrabayu9@gmail.com>
2026-03-05 13:32:35 +0700

    bagian 1

---
+++ Pertemuan2/src/MTitik.java
@@ -0,0 +1,15 @@
+/* Nama File    : Titik.java
+ * Deskripsi    : berisi atribut dan method dalam class Titik
+ * Pembuat      : Ananda Putra Bayu 24060122140125  
+ * Tanggal      : 5/3/2026
+ */
+    public class MTitik {
+        public static void main(String[] args) {
+            Titik T1 = new Titik();
+            T1.setAbsis(3);
+            T1.setordinat(4);
+            T1.printTitik();
+            T1.geser(3, 4);
+            T1.printTitik();
+        }
+    }
